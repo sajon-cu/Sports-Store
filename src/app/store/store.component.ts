8d8725276f02ecbeeeb584de37346f373c09b96d
Sajoncu <saifulislamsajon@gmail.com>
2021-11-26 14:00:06 +0600

    adding product pagination

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,11 +8,16 @@ import { ProductRepository } from "../model/product.repository";
 })
 export class StoreComponent {
     selectedCategory: string = null;
+    public productsPerPage = 10
+    public selectedPage = 1
 
     constructor(private repository: ProductRepository) {}
 
     get products(): Product[] {
-        return this.repository.getProducts(this.selectedCategory);
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+
+        return this.repository.getProducts(this.selectedCategory)
+                            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
 
     get categories(): string[] {
@@ -21,5 +26,20 @@ export class StoreComponent {
 
     changeCategory(newCategory: string) {
         this.selectedCategory = newCategory;
+    }
+
+    changePageSize(newSize:number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    get pageNumbers():number[] {
+        return Array(
+            Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)
+        ).fill(0).map((x, i) => i + 1);
     }
 }
